9e2bb31927fc1fc583e4a160510edd04ebc6363c
Alejandro Garcia <agarciacoding@gmail.com>
2023-01-24 12:53:18 -0600

    i react-icon

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
+import {useState} from 'react' 
 import Header from './components/Header'
 import Tasks from './components/Tasks'
 
 
 const App = () => {
+  const [tasks, setTasks] = useState([
+    {
+        id: 1,
+        text: 'Doctors Appointment',
+        day: 'Feb 4th at 2:30pm',
+        reminder: true,
+    }
+])
   return (
     <div className="container">
      <Header  />
-     <Tasks  />
+     <Tasks tasks = {tasks}  />
     </div>
   );
 }
--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,20 +1,12 @@
-import {useState} from 'react' 
+import Task from './Task'
 
 
 
-const Tasks = () => {
-    const [tasks, setTasks] = useState([
-        {
-            id: 1,
-            text: 'Doctors Appointment',
-            day: 'Feb 4th at 2:30pm',
-            reminder: true,
-        }
-    ])
-  return (
+const Tasks = ({tasks}) => {
+    return (
     <>
     {tasks.map((task)=>(
-        <h3 key={task.id}>{task.text}</h3>
+        <Task key={task.id} task={task}/>
     ))}
     </>
   )
